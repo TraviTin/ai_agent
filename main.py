ef9e9563940e95b9e51a0e575f0c24c10f0f376b
TraviTin <scoutballoons@gmail.com>
2025-11-19 15:33:51 -0600

    added a list to store prompts

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
     load_dotenv()
-    
+
     def user_input():
         if len(sys.argv) > 1:
             return sys.argv[1]
         else:
             print("Error: No input provided. Please provide input as a command-line argument.")
             sys.exit(1)
-        
+
     prompt = user_input()
+
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)]),
+    ]
 
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
 
+    generate_content(client, messages)
+
+def generate_content(client, messages):
     response = client.models.generate_content(
-        model='gemini-2.0-flash-001', contents= prompt)
+        model='gemini-2.0-flash-001', 
+        contents= messages,
+    )
 
     print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
     print("Response tokens: ", response.usage_metadata.candidates_token_count)
