8eac7acff6d56fba4dc32486bcdfaee0da289929
TraviTin <scoutballoons@gmail.com>
2025-11-19 14:55:47 -0600

    small refactor on main

--- main.py
+++ main.py
@@ -1,32 +1,31 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
-import sys
-
-def user_input():
-    if len(sys.argv) > 1:
-        return sys.argv[1]
-    else:
-        print("Error: No input provided. Please provide input as a command-line argument.")
-        sys.exit(1)
-        
-prompt = user_input()
-
-load_dotenv()
-api_key = os.environ.get("GEMINI_API_KEY")
-print("API Key loaded:", api_key)
-client = genai.Client(api_key=api_key)
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents= prompt)
-
-print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
-print("Response tokens: ", response.usage_metadata.candidates_token_count)
-print(response.text)
-
 
 
 def main():
-    print("Hello from ai-agent!")
+    load_dotenv()
+    
+    def user_input():
+        if len(sys.argv) > 1:
+            return sys.argv[1]
+        else:
+            print("Error: No input provided. Please provide input as a command-line argument.")
+            sys.exit(1)
+        
+    prompt = user_input()
+
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash-001', contents= prompt)
+
+    print("Prompt tokens: ", response.usage_metadata.prompt_token_count)
+    print("Response tokens: ", response.usage_metadata.candidates_token_count)
+    print(response.text)
+
 
 
 if __name__ == "__main__":
