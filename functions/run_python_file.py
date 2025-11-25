2a6e17b82bded16736962c17b06a822344f263b6
TraviTin <scoutballoons@gmail.com>
2025-11-24 19:45:26 -0600

    added the ability to run python3 files

---
+++ functions/run_python_file.py
@@ -0,0 +1,28 @@
+import os
+import subprocess
+
+
+def run_python_file(working_directory, file_path, args=[]):
+    full_path = os.path.abspath(os.path.join(working_directory, file_path))
+    wd_abs = os.path.abspath(working_directory)
+    if not full_path.startswith(wd_abs):
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
+    if not os.path.exists(full_path):
+        return f'Error: File "{file_path}" not found.'
+    if not file_path.endswith(".py"):
+        return f'Error: "{file_path}" is not a Python file.'
+    
+    try:
+        result = subprocess.run(["python3", full_path] + args, capture_output=True, cwd=working_directory, timeout=30, text=True) 
+        if len(result.stdout) ==0 and len(result.stderr) == 0:
+            return "No output produced."
+        stdout_only = "STDOUT:" + result.stdout
+        stderr_only = "STDERR:" + result.stderr
+        output = stdout_only + "\n" + stderr_only
+        exit_err = result.returncode
+        if exit_err != 0:
+            output += f"\nProcess exited with code {result.returncode}"
+        return output 
+    
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- functions/write_file.py
+++ functions/write_file.py
@@ -6,7 +6,7 @@ def write_file(working_directory, file_path, content):
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     dir_path = os.path.dirname(abs_file_path)
     if not abs_file_path.startswith(abs_working_dir):
-        return f'Error: Cannot write "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     try:
         if os.path.exists(abs_file_path) and os.path.isdir(abs_file_path):
             return f'Error: "{file_path}" is a directory, not a file'
@@ -17,4 +17,4 @@ def write_file(working_directory, file_path, content):
         return f'Successfully wrote to "{file_path}" ({len(content)} characters written)'
 
     except Exception as e:
-        return f'Error writing file "{file_path}": {e}'
+        return f'Error: {e}'
